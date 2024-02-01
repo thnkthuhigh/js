caee95f7ad860c9b837e0e8cdb63a1e461607e97
Thanh <a01204496068@gmail.com>
2024-02-01 14:42:24 +0700

    call_back

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-var request = new XMLHttpRequest();
-request.onreadystatechange = function () {
-  if (this.readyState == 4) {
-    // Typical action to be performed when the document is ready:
-    console.log(`>>> check rest `, request.responseText);
+const callback = (err, data) => {
+  if (err) {
+    console.log(`call back`, err);
+  }
+  if (data) {
+    console.log(`call back`, data);
   }
 };
-request.open("GET", "https://jsonplaceholder.typicode.com/todos/1", true);
-request.send();
+
+function getTodo() {
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status == 200) {
+      // Typical action to be performed when the document is ready:
+      //   console.log(`>>> check rest `, request);
+      const data = request.responseText;
+      callback(undefined, data);
+    }
+    if (this.readyState === 4 && request.status !== 200) {
+      callback("error", undefined);
+    }
+  };
+  request.open("GET", "https://jsonplaceholder.typicode.com/todos", true);
+  request.send();
+}
+
+getTodo();
