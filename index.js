bac84523a48a84ce0d89aeb5dbc8fd5327300d83
Thanh <a01204496068@gmail.com>
2024-02-01 14:08:08 +0700

    request

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log(`Chao Thanh`);
+var request = new XMLHttpRequest();
+request.onreadystatechange = function () {
+  if (this.readyState == 4) {
+    // Typical action to be performed when the document is ready:
+    console.log(`>>> check rest `, request.responseText);
+  }
+};
+request.open("GET", "https://jsonplaceholder.typicode.com/todos/1", true);
+request.send();
